bdc2907524048361bcc2d88ca790feee422a22ff
gitguti <gitmelgtrrz1@gmail.com>
2023-11-16 12:53:22 -0500

    replace local package to my own npm package

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -4,6 +4,6 @@ import { Demo } from "gitguti-potted-plants-components-starter";
 export const DemoView = () => (
   <div>
     <h1>This is your brand new application scaffold!</h1>
-    {/* <Demo/> */}
+    <Demo/>
   </div>
 );
