b78be451e4d6b640f72cadbaa08adb70f1145155
gitguti <gitmelgtrrz1@gmail.com>
2023-11-16 11:04:45 -0500

    build

---
+++ src/views/DemoView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Demo } from "gitguti-potted-plants-components-starter";
+
+export const DemoView = () => (
+  <div>
+    <h1>This is your brand new application scaffold!</h1>
+    {/* <Demo/> */}
+  </div>
+);
